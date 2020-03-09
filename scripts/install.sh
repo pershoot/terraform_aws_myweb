7621cea09c95515b72912ff43d4e977237a50cdb
Prashant Somashekar <prashsomash@gmail.com>
2020-03-09 07:26:45 -0400

    scripts: remove sudo from apt key add (this is run as SU)

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y python3-pip apt-transport-https ca-certificates curl software-properties-common
 # Install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 apt install -y docker-ce
