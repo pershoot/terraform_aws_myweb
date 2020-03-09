02274435fc2b652b112078c6b3840272e6eb55ab
Prashant Somashekar <prashsomash@gmail.com>
2020-03-09 07:35:57 -0400

    terraform: upgrade

--- lightsail.tf
+++ lightsail.tf
@@ -7,12 +7,12 @@ resource "aws_lightsail_static_ip" "myweb" {
 }
 
 resource "aws_lightsail_instance" "myweb" {
-  name                    = "site_myweb"
-  availability_zone       = "${var.region}a"
-  blueprint_id            = "ubuntu_18_04"
-  bundle_id               = "micro_2_0"
-  key_pair_name           = "myweb"
-  user_data               = "${data.template_file.init_script.rendered}"
+  name              = "site_myweb"
+  availability_zone = "${var.region}a"
+  blueprint_id      = "ubuntu_18_04"
+  bundle_id         = "micro_2_0"
+  key_pair_name     = "myweb"
+  user_data         = data.template_file.init_script.rendered
 
   tags = {
         Site = "myweb.com"
@@ -20,6 +20,6 @@ resource "aws_lightsail_instance" "myweb" {
 }
 
 resource "aws_lightsail_static_ip_attachment" "myweb" {
-  static_ip_name = "${aws_lightsail_static_ip.myweb.name}"
-  instance_name  = "${aws_lightsail_instance.myweb.name}"
+  static_ip_name = aws_lightsail_static_ip.myweb.name
+  instance_name  = aws_lightsail_instance.myweb.name
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "static_public_ip" {
-  value = "${aws_lightsail_static_ip.myweb.ip_address}"
+  value = aws_lightsail_static_ip.myweb.ip_address
 }
