0988df179c9b96d43ab62f4081f714028eed6995
Prashant Somashekar <prashsomash@gmail.com>
2020-03-09 08:18:06 -0400

    terraform: output: switch to pathexpand for hosts file

--- output.tf
+++ output.tf
@@ -4,7 +4,7 @@ output "static_public_ip" {
 
 resource "local_file" "hosts" {
   content              = "[vps]\n${aws_lightsail_static_ip.myweb.ip_address} ansible_connection=ssh ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/myweb instance=${aws_lightsail_instance.myweb.name}"
-  filename             = "${path.module}/../../ansible/hosts"
+  filename             = pathexpand("~/dev/ansible/hosts-aws")
   directory_permission = 0754
   file_permission      = 0664
 }
